33c321f4c38e8365a2f26164e37b67d43fa4babe
Theleeying <3379487792@qq.com>
2026-09-20 17:12:14 +0800

    fix(judge): allow public root-relative URLs

--- src/benchmark/judge/privacy.ts
+++ src/benchmark/judge/privacy.ts
@@ -1,6 +1,6 @@
-const absolutePathTokenPattern = /(^|[^A-Za-z0-9])([A-Za-z]:[\\/][^\s"'`]+|\\\\[^\s"'`]+|\/(?:[A-Za-z0-9._~-]+\/)+[^\s"'`]+)/g;
+const absolutePathTokenPattern = /(^|[^A-Za-z0-9])([A-Za-z]:[\\/][^\s"'`]+|\\\\[^\s"'`]+|\/(?:home|Users|workspace|workspaces|tmp|var|etc|opt|mnt|root|run|proc|dev|usr|bin|sbin|srv|app|private)\/[^\s"'`]+)/gi;
 
-export const absolutePathPattern = /(?:^|[^A-Za-z0-9])(?:[A-Za-z]:[\\/]|\\\\|\/(?:[A-Za-z0-9._~-]+\/)+)/i;
+export const absolutePathPattern = /(?:^|[^A-Za-z0-9])(?:[A-Za-z]:[\\/]|\\\\|\/(?:home|Users|workspace|workspaces|tmp|var|etc|opt|mnt|root|run|proc|dev|usr|bin|sbin|srv|app|private)\/)/i;
 
 export function redactAbsolutePaths(text: string): string {
   return text.replace(absolutePathTokenPattern, "$1[redacted-path]");
